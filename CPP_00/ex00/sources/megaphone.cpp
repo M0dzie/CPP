19a46cbd75733ee0abf674051fd91203f4c09327
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-06-08 14:51:15 +0200

    set up ex00

--- CPP_00/ex00/sources/megaphone.cpp
+++ CPP_00/ex00/sources/megaphone.cpp
@@ -6,14 +6,16 @@
 /*   By: thmeyer < thmeyer@student.42lyon.fr >      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 12:51:23 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/05/18 12:51:54 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/05/25 11:46:28 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <iostream>
 
-int main(void)
+int main(int argc, char **argv)
 {
-	printf("Hello World !\n");
+	if (argc == 1)
+	std::cout << "tamer\n";
 	return (0);
 }
