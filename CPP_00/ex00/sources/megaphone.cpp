c36d97d9f01e9409444611d38e769e3db485542b
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-07-13 02:03:10 +0200

    ex00 finish

--- CPP_00/ex00/sources/megaphone.cpp
+++ CPP_00/ex00/sources/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer < thmeyer@student.42lyon.fr >      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 12:51:23 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/05/25 11:46:28 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/07/13 02:02:03 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,17 @@
 int main(int argc, char **argv)
 {
 	if (argc == 1)
-	std::cout << "tamer\n";
+		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n", 0);
+	for (int i = 1; argv[i]; i++)
+	{
+		for (int j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
+				argv[i][j] -= 32;
+			std::cout << argv[i][j];
+		}
+		if (!argv[i + 1])
+			std::cout << "\n";
+	}
 	return (0);
 }
