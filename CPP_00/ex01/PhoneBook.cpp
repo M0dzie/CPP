8c035e602efb683456f3cb9ec00fa98d33e17490
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-07-20 01:21:13 +0200

    test cpp

--- CPP_00/ex01/PhoneBook.cpp
+++ CPP_00/ex01/PhoneBook.cpp
@@ -1,24 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thmeyer < thmeyer@student.42lyon.fr >      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/14 01:23:42 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/07/19 23:21:33 by thmeyer          ###   ########.fr       */
+/*   Created: 2023/07/19 21:39:06 by thmeyer           #+#    #+#             */
+/*   Updated: 2023/07/19 23:22:37 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 
-int main(int argc, char **argv)
+PhoneBook::PhoneBook()
 {
-	(void) argc;
-	(void) argv;
-	PhoneBook instance;
+	std::cout << "Constructor called" << std::endl;
+	
+	this->foo = 42;
+	std::cout << "this->foo: " << this->foo << std::endl;
 
-	std::cout << "UwU" << std::endl;
-	std::cout << "instance.foo: " << instance.foo << std::endl;
-	return (0);
+	this->bar();
+	return;
+}
+
+PhoneBook::~PhoneBook()
+{
+	std::cout << "Destructor called" << std::endl;
+	return;
+}
+
+void PhoneBook::bar(void)
+{
+	std::cout << "Member function bar called" << std::endl;
+	return;
 }
--- CPP_00/ex01/PhoneBook.hpp
+++ CPP_00/ex01/PhoneBook.hpp
@@ -1,16 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thmeyer < thmeyer@student.42lyon.fr >      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 18:21:47 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/07/17 18:22:16 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/07/19 23:28:03 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
+# include <iostream>
+
+class PhoneBook
+{
+public:
+	int foo;
+
+	void bar(void);
+	
+	PhoneBook(void);
+	~PhoneBook(void);
+};
 
 #endif
