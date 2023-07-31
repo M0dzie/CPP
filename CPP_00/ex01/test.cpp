21141d3832e1dddc6f2a094e83f84a2482d38af0
Modzie <modzielol@gmail.com>
2023-07-31 13:05:26 +0200

    create repo

---
+++ CPP_00/ex01/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	std::cout << "Hello, world !" << std::endl << "omg" << std::endl;
+	std::cout << "Specific char :" << std::endl << "\" and \\" << std::endl;
+	return (0);
+}
