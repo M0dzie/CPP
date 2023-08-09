2f29cb04ae5f9f6661df056fdd8facd97552e070
Modzie <modzielol@gmail.com>
2023-08-09 15:28:43 +0200

    create test dir to make some tests

---
+++ test/test.cpp
@@ -0,0 +1,15 @@
+#include "test.hpp"
+
+int main(int argc, char **argv)
+{
+	Player david, goliath;
+
+	goliath.attack(david);
+	david.drinkHealthPotion(20);
+	goliath.attack(david);
+	david.attack(goliath);
+
+	goliath.switchWeapon("Sahrme", 40);
+	goliath.attack(david);
+	return 0;
+}
