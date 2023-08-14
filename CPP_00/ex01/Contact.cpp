a9e57f565d4e4f72e64543060bbf545cec8fc035
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-08-15 00:21:36 +0200

    set up constrcuctor contact with the correct information to ask

--- CPP_00/ex01/Contact.cpp
+++ CPP_00/ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer < thmeyer@student.42lyon.fr >      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 22:36:09 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/08/15 00:05:25 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/08/15 00:21:10 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,16 @@
 
 Contact::Contact()
 {
-	std::cout << "Please enter the following information :" << std::endl;
+	std::cout << "Please complete the following information :" << std::endl;
 	std::cout << "First name : ";
 	std::cin >> firstName;
+	std::cout << "Last name : ";
+	std::cin >> lastName;
+	std::cout << "nickname : ";
+	std::cin >> nickName;
+	std::cout << "Phone number : ";
+	std::cin >> phoneNumber;
+	std::cout << "Darkest secret : ";
+	std::cin >> darkestSecret;
+	std::cout << "Thanks for your information !" << std::endl;
 }
