e21a122cab2e7db041318e45764ce3a6e908648f
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-08-17 18:34:51 +0200

    init function in Account.cpp

--- CPP_00/ex02/Account.cpp
+++ CPP_00/ex02/Account.cpp
@@ -6,9 +6,60 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 17:33:34 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/08/17 17:36:40 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/08/17 18:32:36 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 
+Account::Account()
+{
+}
+
+Account::Account(int initial_deposit)
+{   
+}
+
+Account::~Account()
+{
+}
+
+static int getNbAccounts()
+{
+}
+
+static int getTotalAmount()
+{
+}
+
+static int getNbDeposits()
+{
+}
+
+static int getNbWithdrawals()
+{
+}
+
+static int displayAccountsInfos()
+{
+}
+
+void Account::makeDeposit(int deposit)
+{
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+}
+
+int Account::checkAmount() const
+{
+}
+
+void Account::displayStatus() const
+{
+}
+
+static void _displayTimeStamp()
+{
+}
