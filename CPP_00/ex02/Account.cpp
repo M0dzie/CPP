7fb922cec945174c5eee53de493abcb3965cf2ce
Modzie <modzielol@gmail.com>
2023-08-21 13:06:29 +0200

    init non function member and var correctly in .cpp

--- CPP_00/ex02/Account.cpp
+++ CPP_00/ex02/Account.cpp
@@ -3,63 +3,79 @@
 /*                                                        :::      ::::::::   */
 /*   Account.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
+/*   By: thmeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 17:33:34 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/08/17 18:32:36 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/08/21 13:05:58 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+void _displayTimeStamp()
+{
+	return;
+}
+
+int Account::getNbAccounts()
+{
+	return 0;
+}
+
+int Account::getTotalAmount()
+{
+	return 0;
+}
+
+int Account::getNbDeposits()
+{
+	return 0;
+}
+
+int Account::getNbWithdrawals()
+{
+	return 0;
+}
+
+void Account::displayAccountsInfos()
+{
+	return;
+}
 
 Account::Account()
 {
 }
 
 Account::Account(int initial_deposit)
-{   
+{
+	(void)initial_deposit;
 }
 
 Account::~Account()
 {
 }
 
-static int getNbAccounts()
-{
-}
-
-static int getTotalAmount()
-{
-}
-
-static int getNbDeposits()
-{
-}
-
-static int getNbWithdrawals()
-{
-}
-
-static int displayAccountsInfos()
-{
-}
-
 void Account::makeDeposit(int deposit)
 {
+	(void)deposit;
 }
 
 bool Account::makeWithdrawal(int withdrawal)
 {
+	(void)withdrawal;
+	return true;
 }
 
 int Account::checkAmount() const
 {
+	return 0;
 }
 
 void Account::displayStatus() const
-{
-}
-
-static void _displayTimeStamp()
 {
 }
