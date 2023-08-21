ce0752f7b959d8863a779c3bb6977eefd933a6aa
Modzie <modzielol@gmail.com>
2023-08-21 14:02:55 +0200

    save for Tday

--- CPP_00/ex02/Account.cpp
+++ CPP_00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 17:33:34 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/08/21 13:05:58 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/08/21 13:39:07 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,39 @@ int	Account::_nbAccounts = 0;
 int	Account::_totalAmount = 0;
 int	Account::_totalNbDeposits = 0;
 int	Account::_totalNbWithdrawals = 0;
+
+Account::Account() : _accountIndex(0), _amount(0), _nbDeposits(0), _nbWithdrawals(0)
+{
+}
+
+Account::Account(int initial_deposit) : _accountIndex(0), _amount(0), _nbDeposits(0), _nbWithdrawals(0)
+{
+	(void)initial_deposit;
+}
+
+Account::~Account()
+{
+}
+
+void Account::makeDeposit(int deposit)
+{
+	(void)deposit;
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+	(void)withdrawal;
+	return true;
+}
+
+int Account::checkAmount() const
+{
+	return this->_amount;
+}
+
+void Account::displayStatus() const
+{
+}
 
 void _displayTimeStamp()
 {
@@ -45,37 +78,4 @@ int Account::getNbWithdrawals()
 void Account::displayAccountsInfos()
 {
 	return;
-}
-
-Account::Account()
-{
-}
-
-Account::Account(int initial_deposit)
-{
-	(void)initial_deposit;
-}
-
-Account::~Account()
-{
-}
-
-void Account::makeDeposit(int deposit)
-{
-	(void)deposit;
-}
-
-bool Account::makeWithdrawal(int withdrawal)
-{
-	(void)withdrawal;
-	return true;
-}
-
-int Account::checkAmount() const
-{
-	return 0;
-}
-
-void Account::displayStatus() const
-{
 }
