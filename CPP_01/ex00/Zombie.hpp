f2c81e51d0f213ba696f2bf2f5ae6aa2be6f3976
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-08-22 17:34:33 +0200

    all files are now created

--- CPP_01/ex00/Zombie.hpp
+++ CPP_01/ex00/Zombie.hpp
@@ -6,21 +6,25 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 17:24:13 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/08/22 17:26:07 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/08/22 17:28:40 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ZOMBIE_HPP
 # define ZOMBIE_HPP
 
+# include <iostream>
+# include <string>
+
 class Zombie
 {
 public:
     Zombie();
     ~Zombie();
+    void announce( void );
 
 private:
-    /* data */
+    std::string name;
 };
 
 #endif
--- CPP_01/ex00/randomChump.cpp
+++ CPP_01/ex00/randomChump.cpp
@@ -1,26 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/22 17:24:32 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/08/22 17:32:27 by thmeyer          ###   ########.fr       */
+/*   Created: 2023/08/22 17:30:13 by thmeyer           #+#    #+#             */
+/*   Updated: 2023/08/22 17:33:48 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie::Zombie()
+void randomChump(std::string name)
 {
-}
-
-Zombie::~Zombie()
-{
-}
-
-void Zombie::announce( void )
-{
-    std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+    Zombie newZombie;
 }
