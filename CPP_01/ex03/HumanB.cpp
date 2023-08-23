b3b68ca083811b37ab103db2c550df1e49d7e27e
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-08-23 20:48:05 +0200

    main fixed

--- CPP_01/ex03/HumanB.cpp
+++ CPP_01/ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 19:08:10 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/08/23 20:42:05 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/08/23 20:45:14 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,5 @@ void HumanB::attack()
 
 void setWeapon(Weapon &weapon)
 {
+    (void)weapon;
 }
--- CPP_01/ex03/main.cpp
+++ CPP_01/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 18:12:05 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/08/23 20:39:07 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/08/23 20:47:40 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,20 @@
 
 int main()
 {
-    Weapon club = Weapon("crude spiked club");
-    HumanA bob("Bob", club);
-    bob.attack();
-    club.setType("some other type of club");
-    bob.attack();
-    Weapon club = Weapon("crude spiked club");
-    HumanB jim("Jim");
-    jim.setWeapon(club);
-    jim.attack();
-    club.setType("some other type of club");
-    jim.attack();
+    {
+        Weapon club = Weapon("crude spiked club");
+        HumanA bob("Bob", club);
+        bob.attack();
+        club.setType("some other type of club");
+        bob.attack();
+    }
+    {
+        Weapon club = Weapon("crude spiked club");
+        HumanB jim("Jim");
+        jim.setWeapon(club);
+        jim.attack();
+        club.setType("some other type of club");
+        jim.attack();
+    }
     return 0;
 }
