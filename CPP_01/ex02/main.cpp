74de687de5474f51d751cd284fec4e3cbcc409d1
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-08-23 17:42:36 +0200

    init ex02

---
+++ CPP_01/ex02/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/23 17:37:03 by thmeyer           #+#    #+#             */
+/*   Updated: 2023/08/23 17:42:08 by thmeyer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+int main()
+{
+    std::string str = "HI THIS IS BRAIN";
+    std::string *stringPTR = &str;
+    std::string &stringREF = str;
+
+    return 0;
+}
