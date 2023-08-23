bfa96a87488bc9d794bbd68f80ec26f6a9ce904e
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-08-23 17:10:41 +0200

    set attribute in private with underscore

--- CPP_01/ex00/Zombie.cpp
+++ CPP_01/ex00/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 17:24:32 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/08/22 17:52:39 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/08/23 17:09:05 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 Zombie::Zombie(std::string name)
 {
-    this->name = name;
+    this->_name = name;
     announce();
 }
 
 Zombie::~Zombie()
 {
-    std::cout << this->name << " found death, sadly." << std::endl;
+    std::cout << this->_name << " found death, sadly." << std::endl;
 }
 
-void Zombie::announce( void )
+void Zombie::announce( void ) const
 {
-    std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+    std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
--- CPP_01/ex00/Zombie.hpp
+++ CPP_01/ex00/Zombie.hpp
@@ -6,13 +6,12 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 17:24:13 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/08/22 17:42:24 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/08/23 17:09:09 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ZOMBIE_HPP
 # define ZOMBIE_HPP
-
 # include <iostream>
 # include <string>
 
@@ -21,10 +20,10 @@ class Zombie
 public:
     Zombie(std::string name);
     ~Zombie();
-    void announce( void );
+    void announce( void ) const;
 
 private:
-    std::string name;
+    std::string _name;
 };
 
 void randomChump(std::string name);
