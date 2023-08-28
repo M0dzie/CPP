65dc039fc39098e8d882ead5b8661e4b54a23a47
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-08-28 15:27:18 +0200

    create func replace with str and pos

--- CPP_01/ex04/File.cpp
+++ CPP_01/ex04/File.cpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 13:12:45 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/08/28 14:58:16 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/08/28 15:26:57 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,16 +32,30 @@ void File::setStrings(std::string s1, std::string s2)
     this->_s2 = s2;
 }
 
+static std::string replace(std::string str, size_t pos)
+{
+    (void)str;
+    (void)pos;
+    std::string newStr;
+
+    return newStr;
+}
+
 void File::createAndCopyFile()
 {
+    size_t pos = 0;
     std::string str;
     std::ifstream infile(this->_name.c_str());
     std::ofstream outfile(this->_replace.c_str());
 
     while (getline(infile, str))
     {
-        if (str.find(this->_s1) <= str.size())
+        pos = str.find(this->_s1);
+        if (pos <= str.size())
+        {
+            str = replace(str, pos);
             std::cout << "found it !" << std::endl;
+        }
         outfile << str << std::endl;
     }
     infile.close();
