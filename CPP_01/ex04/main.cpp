94099ddcc7ef16ff30b34ba750baf620a1c0ccf2
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-08-28 12:59:37 +0200

    display error should be fine

--- CPP_01/ex04/main.cpp
+++ CPP_01/ex04/main.cpp
@@ -6,11 +6,29 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 12:26:01 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/08/28 12:26:09 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/08/28 12:59:18 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main()
+#include <iostream>
+#include <string>
+#include <fstream>
+
+static int checkError(int argc, char **argv)
 {
+    if (argc != 4)
+        return (std::cout << "Program must contain 3 parameters, in order : name of a file, and 2 strings" << std::endl, 1);
+    std::string filename = argv[1];
+    std::string replace = filename + ".replace";
+    std::ifstream inFile(filename.c_str());
+    if (!inFile.good())
+        return (std::cout << "First parameter is not a valid file" << std::endl, 1);
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    if (checkError(argc, argv))
+        return 1;
     return 0;
 }
