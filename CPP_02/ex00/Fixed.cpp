8ccb48a3fffdf48c8e98036f09d751bac55202c3
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-08-29 17:07:07 +0200

    init msg in member function after calling

--- CPP_02/ex00/Fixed.cpp
+++ CPP_02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 15:29:24 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/08/29 16:57:44 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/08/29 17:06:36 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,29 @@
 
 int const Fixed::_nBits = 0;
 
-Fixed::Fixed() : _number(0) {}
+Fixed::Fixed() : _number(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
 
-Fixed::Fixed(const Fixed&) {}
+Fixed::Fixed(const Fixed&)
+{
+    std::cout << "Copy constructor called" << std::endl;
+}
 
-Fixed::~Fixed() {}
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
 
-Fixed& Fixed::operator=(const Fixed&) {}
+Fixed& Fixed::operator=(const Fixed&)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+}
 
 void Fixed::setRawBits( int const raw ) {}
 
-int Fixed::getRawBits( void ) const {}
+int Fixed::getRawBits( void ) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+}
