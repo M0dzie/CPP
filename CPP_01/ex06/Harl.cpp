0e9eb98d41043fdf1aa9a8cd411700e34cab326d
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-08-29 14:28:26 +0200

    clen ex06

--- CPP_01/ex06/Harl.cpp
+++ CPP_01/ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 11:30:04 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/08/29 13:27:17 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/08/29 14:27:27 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,37 +18,52 @@ Harl::~Harl() {}
 
 void Harl::complain(std::string level)
 {
+    int i;
     std::string arrLevel[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-    void (Harl::*funcPtr[4])() const = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
 
-    for (int i = 0; i < 4; i++)
+    for (i = 0; i < 4; i++)
         if (arrLevel[i] == level)
-            (this->*(funcPtr[i]))();
+            break;
+    switch (i)
+    {
+    case 0:
+        this->debug();
+    case 1:
+        this->info();
+    case 2:
+        this->warning();
+    case 3:
+        this->error();
+        break;
+    default:
+        std::cout << "Unknown command !" << std::endl;
+        break;
+    }
 }
 
 void Harl::debug() const
 {
-    std::cout << "--- DEBUG ---" << std::endl;
+    std::cout << "[ DEBUG ]" << std::endl;
     std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. \
 I really do !" << std::endl;
 }
 
 void Harl::info() const
 {
-    std::cout << "--- INFO ---" << std::endl;
+    std::cout << "[ INFO ]" << std::endl;
     std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger ! \
 If you did, I wouldn’t be asking for more !" << std::endl;
 }
 
 void Harl::warning() const
 {
-    std::cout << "--- WARNING ---" << std::endl;
+    std::cout << "[ WARNING ]" << std::endl;
     std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas \
 you started working here since last month" << std::endl;
 }
 
 void Harl::error() const
 {
-    std::cout << "--- ERROR ---" << std::endl;
+    std::cout << "[ ERROR ]" << std::endl;
     std::cout << "This is unacceptable ! I want to speak to the manager now." << std::endl;
 }
--- CPP_01/ex06/main.cpp
+++ CPP_01/ex06/main.cpp
@@ -6,18 +6,18 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 11:28:43 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/08/29 13:26:40 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/08/29 14:26:54 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
     Harl harl;
-    std::string randLevel[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-    
-    std::srand(std::time(NULL));
-    harl.complain(randLevel[std::rand() % 4]);
+
+    if (argc != 2)
+        return (std::cerr << "Harlfilter take only 1 argument" << std::endl, 1);
+    harl.complain(argv[1]);
     return 0;
 }
