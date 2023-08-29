9d3b602a6742a79dea98d9c874257c9dcb5729aa
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-08-29 14:39:11 +0200

    all fix for push

--- CPP_01/ex00/Zombie.cpp
+++ CPP_01/ex00/Zombie.cpp
@@ -6,15 +6,14 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 17:24:32 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/08/23 17:09:05 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/08/29 14:31:52 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie::Zombie(std::string name)
+Zombie::Zombie(std::string name) : _name(name)
 {
-    this->_name = name;
     announce();
 }
 
--- CPP_01/ex01/Zombie.cpp
+++ CPP_01/ex01/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 17:24:32 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/08/28 13:13:52 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/08/29 14:32:52 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,8 @@
 
 Zombie::Zombie() {}
 
-Zombie::Zombie(std::string name)
+Zombie::Zombie(std::string name) : _name(name)
 {
-    this->_name = name;
     announce();
 }
 
