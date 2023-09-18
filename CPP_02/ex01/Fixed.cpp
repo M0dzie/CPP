239dbae5c699fdd2b84deabd8363b091654e8f35
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-09-18 13:43:04 +0200

    bitwise finished, stocked like a comma step to right and display by step to the left

--- CPP_02/ex01/Fixed.cpp
+++ CPP_02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 09:40:11 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/09/18 10:45:06 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/09/18 13:41:30 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,21 @@
 
 int const Fixed::_nFractionalBits = 8;
 
-Fixed::Fixed()
+Fixed::Fixed() : _fixedPointNumberValue(0)
 {
-    std::cout << "Defaut constructor called" << std::endl;
+    std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(int const newInt) : _fixedPointNumberValue(newInt)
+Fixed::Fixed(int const newInt)
 {
     std::cout << "Int constructor called" << std::endl;
+    this->setRawBits(newInt << Fixed::_nFractionalBits);
+}
+
+Fixed::Fixed(float const newFloat)
+{
+    std::cout << "Float constructor called" << std::endl;
+    this->setRawBits(roundf(newFloat * (1 << Fixed::_nFractionalBits)));
 }
 
 Fixed::Fixed(Fixed const &src)
@@ -33,10 +40,37 @@ Fixed::Fixed(Fixed const &src)
 Fixed::~Fixed()
 {
     std::cout << "Destructor called" << std::endl;
-}   
+}
 
-Fixed &Fixed::operator=(Fixed const &)
+Fixed &Fixed::operator=(Fixed const &src)
 {
     std::cout << "Copy assignment operator called" << std::endl;
+    this->setRawBits(src.getRawBits());
     return *this;
+}
+
+std::ostream &operator<<(std::ostream &outPut, Fixed const &src)
+{
+    outPut << src.toFloat();
+    return outPut;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    this->_fixedPointNumberValue = raw;
+}
+
+int Fixed::getRawBits(void) const
+{
+    return this->_fixedPointNumberValue;
+}
+
+int Fixed::toInt(void) const
+{
+    return (this->getRawBits() >> Fixed::_nFractionalBits);
+}
+
+float Fixed::toFloat(void) const
+{
+    return ((float)this->getRawBits() / (1 << Fixed::_nFractionalBits));
 }
--- CPP_02/ex01/Fixed.hpp
+++ CPP_02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 09:30:45 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/09/18 10:45:06 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/09/18 13:28:04 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,20 @@ class Fixed
 public:
     Fixed();
     Fixed(int const newInt);
+    Fixed(float const newFloat);
     Fixed(Fixed const &src);
     ~Fixed();
-    Fixed &operator=(Fixed const &);
+    Fixed &operator=(Fixed const &src);
+    void setRawBits(int const raw);
+    int getRawBits(void) const;
+    int toInt(void) const;
+    float toFloat(void) const;
 
 private:
     int _fixedPointNumberValue;
     static int const _nFractionalBits;
 };
+
+std::ostream &operator<<(std::ostream &outPut, Fixed const &src);
 
 #endif
