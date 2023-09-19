e7b01431aa0de0e3a003eaf13a693450af1f76ef
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-09-19 17:00:25 +0200

    init ex01

---
+++ CPP_03/ex01/ClapTrap.cpp
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 10:39:58 by thmeyer           #+#    #+#             */
+/*   Updated: 2023/09/19 16:54:31 by thmeyer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int ClapTrap::alive(1);
+
+ClapTrap::ClapTrap() : _name("Unknown"), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+	std::cout << GREEN << "Default constructor called" << RESET << std::endl;
+}
+
+ClapTrap::ClapTrap(std::string name) : _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+	std::cout << GREEN << BOLD << "Overloaded constructor called" << RESET << std::endl;
+}
+
+ClapTrap::ClapTrap(ClapTrap const &src)
+{
+	std::cout << BLUE << "Copy constructor called" << RESET << std::endl;
+	*this = src;
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << RED << "Destructor called" << RESET << std::endl;
+}
+
+ClapTrap &ClapTrap::operator=(ClapTrap const &)
+{
+	std::cout << YELLOW << "Copy assignment operator called" << RESET << std::endl;
+	return *this;
+}
+
+void ClapTrap::attack(const std::string& target)
+{
+	if (this->_hitPoints < 1)
+	{
+		std::cout << BLACK << "ClapTrap " << this->_name << " can't attack... I mean, he's dead." << RESET << std::endl;
+		return;
+	}
+	if (!ClapTrap::alive)
+		return ;
+	if (this->_energyPoints < 1)
+	{
+		std::cout << BLACK << "ClapTrap " << this->_name << " doesn't have enough energy points to attack !" << RESET << std::endl;
+		return ;
+	}
+	this->_energyPoints--;
+	std::cout << "ClapTrap " << this->_name << " attacks " << target << ", causing " << this->_attackDamage << " points of damage!"
+	<< std::endl;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (!ClapTrap::alive)
+		return ;
+	this->_hitPoints -= amount;
+	if (this->_hitPoints < 0)
+		this->_hitPoints = 0;
+	std::cout << "ClapTrap " << this->_name << " lost " << amount << " points of health!" << std::endl;
+	if (this->_hitPoints < 1)
+	{
+		std::cout << RED << BOLD << "ClapTrap " << this->_name << " died." << RESET << std::endl;
+		ClapTrap::alive = 0;
+	}
+	else
+		std::cout << this->_name << " still have " << this->_hitPoints << "hp left!" << std::endl;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (this->_hitPoints < 1)
+	{
+		std::cout << BLACK << "ClapTrap " << this->_name << " is dead, leave him alone, he can't be repaired." << RESET << std::endl;
+		return;
+	}
+	if (this->_energyPoints < 1)
+	{
+		std::cout << BLACK << "ClapTrap " << this->_name << " doesn't have enough energy points to attack !" << RESET << std::endl;
+		return ;
+	}
+	this->_energyPoints--;
+	this->_hitPoints += amount;
+	std::cout << "ClapTrap " << this->_name << " earned " << amount << " points of health!" << std::endl;
+}
+
+void ClapTrap::setAttackDamage(unsigned int amount)
+{
+	this->_attackDamage = amount;
+}
+
+std::string ClapTrap::getName()
+{
+	return this->_name;
+}
+
+int ClapTrap::getAttackDamage()
+{
+	return this->_attackDamage;
+}
+
+int ClapTrap::getHitPoints()
+{
+	return this->_hitPoints;
+}
---
+++ CPP_03/ex01/ClapTrap.hpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 10:37:03 by thmeyer           #+#    #+#             */
+/*   Updated: 2023/09/19 16:52:29 by thmeyer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+# include <iostream>
+# include <string>
+
+# define BLACK "\033[0;30m"
+# define RED "\033[0;31m"
+# define GREEN "\033[0;32m"
+# define YELLOW "\033[0;33m"
+# define BLUE "\033[0;34m"
+# define PURPLE "\033[0;35m"
+# define CYAN "\033[0;36m"
+# define RESET "\033[0m" 
+# define BOLD "\033[1m"
+
+class ClapTrap
+{
+public:
+    ClapTrap();
+    ClapTrap(std::string name);
+    ClapTrap(ClapTrap const &src);
+    ClapTrap &operator=(ClapTrap const &);
+    ~ClapTrap();
+
+	void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+	void setAttackDamage(unsigned int amount);
+	std::string getName();
+	int getAttackDamage();
+	int getHitPoints();
+
+private:
+	std::string _name;
+	int _hitPoints;
+	int _energyPoints;
+	int _attackDamage;
+
+	static int alive;
+};
+
+#endif
