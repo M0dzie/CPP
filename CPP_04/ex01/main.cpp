c1dd79ad7a401c4da868cef0214431142a122674
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-10-02 10:20:37 +0200

    init array of dog and cats, then delete it without leaks

--- CPP_04/ex01/main.cpp
+++ CPP_04/ex01/main.cpp
@@ -19,8 +19,15 @@ int main (void)
 {
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
-
 	delete j;//should not create a leak
 	delete i;
+
+	Animal *array[10];
+	for (int i = 0; i < 5; i++)
+		array[i] = new Dog();
+	for(int i = 5; i < 10; i++)
+		array[i] = new Cat();
+	for (int i = 0; i < 10; i++)
+		delete array[i];
 	return 0;
 }
