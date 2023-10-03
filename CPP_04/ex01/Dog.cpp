1c42e6c5d924c81c997bc956fe27b2fde5d5d16a
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-10-03 10:18:36 +0200

    change dog file to remove useless malloc

--- CPP_04/ex01/Dog.cpp
+++ CPP_04/ex01/Dog.cpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 11:08:41 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/10/02 15:15:25 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/10/03 10:17:09 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ Dog::Dog() : Animal()
 
 Dog::Dog(Dog const &rhs) : Animal(rhs)
 {
-	this->_brain = new Brain();
 	std::cout << "Dog copy constructor called" << std::endl;
 	this->type = "Dog";
 	*this = rhs;
@@ -34,7 +33,6 @@ Dog &Dog::operator=(Dog const &rhs)
 		Animal::operator=(rhs);
 		std::cout << "Dog copy assignement operator called" << std::endl;
 		this->type = rhs.type;
-		delete this->_brain;
 		this->_brain = new Brain(*rhs._brain);
 	}
 	return *this;
