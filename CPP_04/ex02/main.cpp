6a103176af9359579e1f667ee185cec2fd719a1f
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-10-03 09:46:43 +0200

    add beter main

--- CPP_04/ex02/main.cpp
+++ CPP_04/ex02/main.cpp
@@ -15,10 +15,54 @@
 
 int main (void)
 {
-	const AAnimal* testCat = new Cat();
-	const AAnimal* testDog = new Dog();
-	delete testCat;
-	delete testDog;
+	const AAnimal* j = new Dog();
+	const AAnimal* i = new Cat();
+
+	const AAnimal *copy = i;
+	std::cout << "Copy type : " << copy->getType() << std::endl;
+	copy->makeSound();
+	copy = j;
+	std::cout << "Copy type : " << copy->getType() << std::endl;
+	copy->makeSound();
+
+	delete j;//should not create a leak
+	delete i;
+
+	AAnimal *array[10];
+	for (int i = 0; i < 5; i++)
+		array[i] = new Dog();
+	for(int i = 5; i < 10; i++)
+		array[i] = new Cat();
+	copy = array[2];
+	std::cout << "Copy type : " << copy->getType() << std::endl;
+	copy->makeSound();
+	copy = array[9];
+	std::cout << "Copy type : " << copy->getType() << std::endl;
+	copy->makeSound();
+	for (int i = 0; i < 10; i++)
+		delete array[i];
+	
+	Cat cat;
+	cat.setBrainIdeas("Cat's ideas", 0);
+	cat.setBrainIdeas("Cat's first idea", 1);
+	cat.setBrainIdeas("Cat's last idea", 99);
+	Cat catCopy = cat;
+	std::cout << GRAY << BOLD << "CatCopy's brain" << RESET << std::endl;
+	std::cout << catCopy.getBrainIdeas(0) << std::endl;
+	std::cout << catCopy.getBrainIdeas(1) << std::endl;
+	std::cout << catCopy.getBrainIdeas(99) << std::endl;
+
+	catCopy.setBrainIdeas("CatCopy's ideas", 0);
+	catCopy.setBrainIdeas("CatCopy's first ideas", 1);
+	catCopy.setBrainIdeas("CatCopy's last ideas", 99);
+	std::cout << GRAY << BOLD << "Cat's brain" << RESET << std::endl;
+	std::cout << cat.getBrainIdeas(0) << std::endl;
+	std::cout << cat.getBrainIdeas(1) << std::endl;
+	std::cout << cat.getBrainIdeas(99) << std::endl;
+	std::cout << GRAY << BOLD << "CatCopy's brain" << RESET << std::endl;
+	std::cout << catCopy.getBrainIdeas(0) << std::endl;
+	std::cout << catCopy.getBrainIdeas(1) << std::endl;
+	std::cout << catCopy.getBrainIdeas(99) << std::endl;
 
 	// Should NOT compile
 	// const AAnimal* testAanimal = new AAnimal();
