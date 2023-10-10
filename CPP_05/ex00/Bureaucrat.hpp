5ddb622405ad4179ff4d704d2e6a2578ca7abc27
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-10-10 14:26:52 +0200

    init ex00

--- CPP_05/ex00/Bureaucrat.hpp
+++ CPP_05/ex00/Bureaucrat.hpp
@@ -1,16 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/10 14:17:26 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/10/10 14:17:37 by thmeyer          ###   ########.fr       */
+/*   Created: 2023/10/10 14:19:23 by thmeyer           #+#    #+#             */
+/*   Updated: 2023/10/10 14:26:28 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(void)
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+# include <string>
+
+class Bureaucrat
 {
-    return 0;
-}
+public:
+    Bureaucrat();
+    Bureaucrat(Bureaucrat const &rhs);
+    Bureaucrat &operator=(Bureaucrat const &rhs);
+    ~Bureaucrat();
+
+private:
+    std::string const _name;
+    int _grade;
+};
+
+std::ostream &operator<<(std::ostream output, Bureaucrat const &rhs);
+
+#endif
