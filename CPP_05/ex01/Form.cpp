c8c260958b85fe35fd4ee2ead40b9e7e62d9b5d6
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-10-11 15:27:02 +0200

    add tests to main but still need to handle issues with form

--- CPP_05/ex01/Form.cpp
+++ CPP_05/ex01/Form.cpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 13:46:55 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/10/11 15:09:22 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/10/11 15:26:22 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,12 +57,16 @@ void Form::beSigned(Bureaucrat &who)
     try
     {
         if (who.getGrade() <= this->_gradeToSign)
+        {
             this->_signed = TRUE;
+            std::cout << this->_name << " could be signed by " << who.getName() << std::endl;
+        }
         else
             throw Form::GradeTooLowException();
     }
     catch(const Form::GradeTooLowException &e)
     {
+        std::cerr << RED << this->_name << " couldn't be signed by " << who.getName() << " :" << std::endl;
         std::cerr << RED << e.what() << std::endl;
     }
 }
--- CPP_05/ex01/main.cpp
+++ CPP_05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 14:17:26 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/10/11 15:15:15 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/10/11 15:24:04 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,26 @@ int main(void)
     Bureaucrat good("Thomas", 10);
     Bureaucrat bad("Mehdi", 145);
     Form file1("Taxes", 50, 50);
-    Form file2("Salary", 1, 1);
+    Form file2("Salary", 10, 10);
 
     std::cout << YELLOW << BOLD << "---- DISPLAY BUREAUCRAT ----" << RESET << std::endl;
     std::cout << good << bad << std::endl;
     std::cout << YELLOW << BOLD << "---- DISPLAY FORM ----" << RESET << std::endl;
-    std::cout << file1 << std::endl << file2;
+    std::cout << file1 << std::endl << file2 << std::endl;
+
+    file1.beSigned(good);
+    file1.beSigned(bad);
+    file2.beSigned(good);
+    file2.beSigned(bad);
+    std::cout << std::endl<< YELLOW << BOLD << "---- DISPLAY FORM ----" << RESET << std::endl;
+    std::cout << file1 << std::endl << file2 << std::endl;
+
+    std::cout << std::endl;
+
+    good.signForm(file1);
+    good.signForm(file2);
+    bad.signForm(file1);
+    bad.signForm(file2);
     
     return 0;
 }
