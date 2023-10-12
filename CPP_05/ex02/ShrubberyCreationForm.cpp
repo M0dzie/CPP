b545d26660c867a68ab0c1aacd0ca67ff2b68437
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-10-12 14:00:51 +0200

    add exception to shrubbery class and works fine with grade too low

--- CPP_05/ex02/ShrubberyCreationForm.cpp
+++ CPP_05/ex02/ShrubberyCreationForm.cpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 10:49:27 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/10/12 13:29:05 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/10/12 13:56:47 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,24 @@ ShrubberyCreationForm::ShrubberyCreationForm()
 
 ShrubberyCreationForm::ShrubberyCreationForm(AForm const &target)
 {
-    std::string name = target.getName() + "_shrubbery";
+    try
+    {
+        if (target.getGradeToSign() >= 145)
+            throw AForm::GradeTooLowException();
+        else if  (target.getGradeToExec() >= 137)
+            throw AForm::GradeTooLowException();
+    }
+    catch(const AForm::GradeTooLowException &e)
+    {
+        if (target.getGradeToSign() >= 145)
+            std::cerr << RED << "Grade to Sign issues :" << RESET << std::endl;
+        else
+            std::cerr << RED << "Grade to Exec issues :" << RESET << std::endl;
+        std::cerr << RED << e.what() << RESET << std::endl;
+        return;
+    }
+
+    std::string name = target.getName() + "_shrubbery"; 
     std::ofstream outfile(name.c_str());
     
     outfile << "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡤⠤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⠤⢤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡤⠤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⠤⢤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀" << std::endl
@@ -73,6 +90,7 @@ ShrubberyCreationForm::ShrubberyCreationForm(AForm const &target)
     << "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠒⠾⡇⠀⠈⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠒⠾⡇⠀⠈⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀" << std::endl
     << "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⣢⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⣢⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" << std::endl;
     outfile.close();
+    std::cout << "File " << name << " created" << std::endl;
 }
 
 ShrubberyCreationForm::ShrubberyCreationForm(ShrubberyCreationForm const &rhs)
