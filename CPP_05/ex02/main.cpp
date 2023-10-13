14a38750825525454aa12ff0041ca41b5c61a7d0
Modzie <modzielol@gmail.com>
2023-10-13 09:42:18 +0200

    setup main for this exercice

--- CPP_05/ex02/main.cpp
+++ CPP_05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 14:17:26 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/10/13 09:26:34 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/10/13 09:41:38 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,36 +17,27 @@
 
 int main(void)
 {
-    Bureaucrat good("Thomas", 10);
-    Bureaucrat bad("Mehdi", 14);
-    AForm file1("Taxes", 150, 50);
-    AForm file2("Salary", 23, 10);
-    AForm filePres("President", 10, 5);
+    AForm home("Home", 75, 75);
+    AForm badHome("BadHome", 145, 138);
+    AForm patient("Patient", 25, 45);
+    AForm badPatient("BadPatient", 73, 44);
+    AForm mehdi("Mehdi", 25, 5);
+    AForm clement("Clement", 26, 7);
 
-    std::cout << YELLOW << BOLD << "---- DISPLAY BUREAUCRAT ----" << RESET << std::endl;
-    std::cout << good << bad << std::endl;
-    std::cout << YELLOW << BOLD << "---- DISPLAY FORM ----" << RESET << std::endl;
-    std::cout << file1 << std::endl << file2 << std::endl;
-
-    std::cout << YELLOW << BOLD << "---- THOMAS ----" << RESET << std::endl;
-    file1.beSigned(good);
-    file2.beSigned(good);
-    std::cout << std::endl<< YELLOW << BOLD << "---- DISPLAY FORM ----" << RESET << std::endl;
-    std::cout << file1 << std::endl << file2 << std::endl;
-    good.signForm(file1);
-    good.signForm(file2);
-
-    std::cout << std::endl << YELLOW << BOLD << "---- MEHDI ----" << RESET << std::endl;
-    file1.beSigned(bad);
-    file2.beSigned(bad);
-    std::cout << std::endl<< YELLOW << BOLD << "---- DISPLAY FORM ----" << RESET << std::endl;
-    std::cout << file1 << std::endl << file2 << std::endl;
-    bad.signForm(file1);
-    bad.signForm(file2);
-
-    ShrubberyCreationForm test(file1);
-    RobotomyRequestForm test2(file2);
-    PresidentialPardonForm test3(filePres);
+    std::cout << YELLOW << BOLD << "---- SHRUBBERY FORM ----" << RESET << std::endl;
+    ShrubberyCreationForm test(home);
+    ShrubberyCreationForm test1(badHome);
+    std::cout << std::endl;
+    
+    std::cout << YELLOW << BOLD << "---- ROBOTOMY FORM ----" << RESET << std::endl;
+    RobotomyRequestForm test2(patient);
+    RobotomyRequestForm test3(badPatient);
+    std::cout << std::endl;
+    
+    std::cout << YELLOW << BOLD << "---- PRESIDENTIAL FORM ----" << RESET << std::endl;
+    PresidentialPardonForm test4(mehdi);
+    PresidentialPardonForm test5(clement);
+    std::cout << std::endl;
 
     return 0;
 }
