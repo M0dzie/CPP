0a170c665e074736edb3d0976d0dff87da261c59
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-10-17 15:40:43 +0200

    init basic files for ex00

---
+++ CPP_06/ex00/ScalarConverter.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/17 15:33:40 by thmeyer           #+#    #+#             */
+/*   Updated: 2023/10/17 15:39:11 by thmeyer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+# include <iostream>
+
+# define BLACK "\033[0;30m"
+# define RED "\033[0;31m"
+# define GREEN "\033[0;32m"
+# define YELLOW "\033[0;33m"
+# define BLUE "\033[0;34m"
+# define PURPLE "\033[0;35m"
+# define CYAN "\033[0;36m"
+# define GRAY "\033[0;37m"
+# define RESET "\033[0m" 
+# define BOLD "\033[1m"
+
+class ScalarConverter
+{
+public:
+    ScalarConverter();
+    ScalarConverter(ScalarConverter const &rhs);
+    ScalarConverter &operator=(ScalarConverter const &rhs);
+    ~ScalarConverter();
+
+private:
+};
+
+#endif
