2e2325551b951cf8065c9ae2c80bcc448992459b
Modzie <modzielol@gmail.com>
2023-10-22 19:57:35 +0200

    toInt finish

--- CPP_06/ex00/ScalarConverter.cpp
+++ CPP_06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 15:45:44 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/10/22 17:06:30 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/10/22 19:55:31 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,6 +75,11 @@ void ScalarConverter::toInt(std::string input)
     std::cout << "int: ";
     if (isPseudoLiterals(input, INT))
         return;
+    long long intInput = std::atoll(input.c_str());
+    if (intInput < INT_MIN || intInput > INT_MAX)
+        std::cout << "impossible" << std::endl;
+    else
+        std::cout << static_cast<int>(intInput) << std::endl;
 }
 
 void ScalarConverter::toFloat(std::string input)
@@ -82,6 +87,7 @@ void ScalarConverter::toFloat(std::string input)
     std::cout << "float: ";
     if (isPseudoLiterals(input, FLOAT))
         return;
+    float floatInput = std::atof(input.c_str());
 }
 
 void ScalarConverter::toDouble(std::string input)
