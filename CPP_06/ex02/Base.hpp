c96ef0362930734b2b162fead79eff9d3c8595ff
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-10-23 18:59:26 +0200

    CPP06 done

--- CPP_06/ex02/Base.hpp
+++ CPP_06/ex02/Base.hpp
@@ -6,12 +6,27 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 15:57:02 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/10/23 16:42:56 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/10/23 18:57:50 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BASE_HPP
 # define BASE_HPP
+# include <iostream>
+# include <ctime>
+# include <cstdlib>
+# include <unistd.h>
+
+# define BLACK "\033[0;30m"
+# define RED "\033[0;31m"
+# define GREEN "\033[0;32m"
+# define YELLOW "\033[0;33m"
+# define BLUE "\033[0;34m"
+# define PURPLE "\033[0;35m"
+# define CYAN "\033[0;36m"
+# define GRAY "\033[0;37m"
+# define RESET "\033[0m" 
+# define BOLD "\033[1m"
 
 class Base
 {
--- CPP_06/ex02/main.cpp
+++ CPP_06/ex02/main.cpp
@@ -6,13 +6,76 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 15:55:57 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/10/23 16:43:15 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/10/23 18:59:00 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Base.hpp"
 
+Base *generate(void)
+{
+    std::srand(std::time(NULL));
+    int random = rand() % 3;
+
+    if (random == 0)
+        return (std::cout << BLACK << "Generation of an instance type A" << RESET << std::endl, \
+        new A());
+    else if (random == 1)
+        return (std::cout << BLACK << "Generation of an instance type B" << RESET << std::endl, \
+        new B());
+    else
+        return (std::cout << BLACK << "Generation of an instance type C" << RESET << std::endl, \
+        new C());
+}
+
+void identify(Base *p)
+{
+    if (A *a = dynamic_cast<A*>(p))
+        std::cout << GREEN << BOLD << "The type of this instance is A with pointer !" << RESET << std::endl;
+    else if (B *b = dynamic_cast<B*>(p))
+        std::cout << GREEN << BOLD << "The type of this instance is B with pointer !" << RESET << std::endl;
+    else if (C *c = dynamic_cast<C*>(p))
+        std::cout << GREEN << BOLD << "The type of this instance is C with pointer !" << RESET << std::endl;
+}
+
+void identify(Base &p)
+{
+    try
+    {
+        A &a = dynamic_cast<A&>(p);
+        (void)a;
+        std::cout << GREEN << BOLD << "The type of this instance is A with reference !" << RESET << std::endl;
+    }
+    catch (...) {}
+    try
+    {
+        B &b = dynamic_cast<B&>(p);
+        (void)b;
+        std::cout << GREEN << BOLD << "The type of this instance is B with reference !" << RESET << std::endl;
+    }
+    catch (...) {}
+    try
+    {
+        C &c = dynamic_cast<C&>(p);
+        (void)c;
+        std::cout << GREEN << BOLD << "The type of this instance is C with reference !" << RESET << std::endl;
+    }
+    catch (...) {}
+}
+
 int main(void)
 {
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << YELLOW << BOLD << "---- TEST " << i << " ----" << RESET << std::endl;
+        {
+            Base *test = generate();
+            identify(test);
+            identify(*test);
+            delete test;
+        }
+        if (i != 4)
+            sleep(1);
+    }
     return 0;
 }
