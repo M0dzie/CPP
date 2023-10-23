8a10f81fbace9f7631816b16628543aca536ea7c
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-10-23 10:27:30 +0200

    string to sthg not available in c++ 98

--- CPP_06/ex00/ScalarConverter.hpp
+++ CPP_06/ex00/ScalarConverter.hpp
@@ -13,6 +13,8 @@
 #ifndef SCALARCONVERTER_HPP
 # define SCALARCONVERTER_HPP
 # include <iostream>
+# include <string>
+# include <stdlib.h>
 
 # define BLACK "\033[0;30m"
 # define RED "\033[0;31m"
@@ -29,6 +31,9 @@
 # define INT 1
 # define FLOAT 2
 # define DOUBLE 3
+
+# define INT_MIN -2147483648
+# define INT_MAX 2147483647
 
 class ScalarConverter
 {
