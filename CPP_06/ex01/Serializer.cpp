582f889c2e6067d9bbe6ef5f89ce330eb60b2491
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-10-23 15:51:59 +0200

    fix leaks in main

--- CPP_06/ex01/Serializer.cpp
+++ CPP_06/ex01/Serializer.cpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 13:42:13 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/10/23 15:27:25 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/10/23 15:49:39 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,10 @@ Serializer::~Serializer() {}
 
 uintptr_t Serializer::serialize(Data *ptr)
 {
-    uintptr_t raw = reinterpret_cast<uintptr_t>(ptr);
-    return raw;
+    return reinterpret_cast<uintptr_t>(ptr);
 }
 
 Data *Serializer::deserialize(uintptr_t raw)
 {
-    Data *ptr = reinterpret_cast<Data*>(raw);
-    return ptr;
+    return reinterpret_cast<Data*>(raw);
 }
--- CPP_06/ex01/main.cpp
+++ CPP_06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 13:40:37 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/10/23 15:44:08 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/10/23 15:51:27 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 int main(void)
 {
     Data *data = new Data();
-    Data *datacpy = new Data();
 
     std::cout << YELLOW << BOLD << "---- INIT DATA ----" << RESET << std::endl;
     data->name = "Mehdi";
@@ -25,27 +24,18 @@ int main(void)
     std::cout << BLACK << "---- DISPLAY DATA ----" << RESET << std::endl;
     std::cout << "Name : " << data->name << std::endl << "Age : " << data->age
     << std::endl << "Gender : " << data->gender << std::endl << std::endl;
-    
-    std::cout << YELLOW << BOLD << "---- INIT DATACPY ----" << RESET << std::endl;
-    datacpy->name = "Unknown";
-    datacpy->age = 0;
-    datacpy->gender = "something";
-    std::cout << GREEN << BOLD << "OK !" << RESET << std::endl;
-    std::cout << BLACK << "---- DISPLAY DATACPY ----" << RESET << std::endl;
-    std::cout << "Name : " << datacpy->name << std::endl << "Age : " << datacpy->age
-    << std::endl << "Gender : " << datacpy->gender << std::endl << std::endl;
 
     std::cout << YELLOW << BOLD << "---- SERIALIZE ----" << RESET << std::endl;
     uintptr_t dataptr = Serializer::serialize(data);
     std::cout << GREEN << BOLD << "OK !" << RESET << std::endl << std::endl;
 
     std::cout << YELLOW << BOLD << "---- DESERIALIZE ----" << RESET << std::endl;
-    datacpy = Serializer::deserialize(dataptr);
-    std::cout << GREEN << BOLD << "OK !" << RESET << std::endl << std::endl;
+    Data *datacpy = Serializer::deserialize(dataptr);
+    std::cout << GREEN << BOLD << "OK !" << RESET << std::endl;
     
     std::cout << BLACK << "---- DISPLAY DATA ----" << RESET << std::endl;
     std::cout << "Name : " << data->name << std::endl << "Age : " << data->age
-    << std::endl << "Gender : " << data->gender << std::endl << std::endl;
+    << std::endl << "Gender : " << data->gender << std::endl;
     std::cout << BLACK << "---- DISPLAY DATACPY ----" << RESET << std::endl;
     std::cout << "Name : " << datacpy->name << std::endl << "Age : " << datacpy->age
     << std::endl << "Gender : " << datacpy->gender << std::endl;
