9f285f00b04be401cae760c59e3b160475fe9db6
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-10-23 16:43:57 +0200

    delete useless files, just need Base.hpp

--- CPP_06/ex02/Base.hpp
+++ CPP_06/ex02/Base.hpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 15:57:02 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/10/23 15:57:31 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/10/23 16:42:56 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,11 @@ class Base
 {
 private:
 public:
-    Base();
-    virtual ~Base();
+    virtual ~Base() {};
 };
+
+class A : public Base {};
+class B : public Base {};
+class C : public Base {};
 
 #endif
