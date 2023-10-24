eb239047293cbcaadc9bbb14f2172d9b9a9cb87c
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-10-24 11:46:02 +0200

    init files for ex00

--- CPP_07/ex00/whatever.hpp
+++ CPP_07/ex00/whatever.hpp
@@ -1,18 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/24 11:45:29 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/10/24 11:45:38 by thmeyer          ###   ########.fr       */
+/*   Created: 2023/10/24 11:44:24 by thmeyer           #+#    #+#             */
+/*   Updated: 2023/10/24 11:44:42 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "whatever.hpp"
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
 
-int main(void)
-{
-    return 0;
-}
+# define BLACK "\033[0;30m"
+# define RED "\033[0;31m"
+# define GREEN "\033[0;32m"
+# define YELLOW "\033[0;33m"
+# define BLUE "\033[0;34m"
+# define PURPLE "\033[0;35m"
+# define CYAN "\033[0;36m"
+# define GRAY "\033[0;37m"
+# define RESET "\033[0m" 
+# define BOLD "\033[1m"
+
+
+
+#endif
