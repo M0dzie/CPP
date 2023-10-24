b3c72532435b4a146f078148a1ce1adb539d15a6
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-10-24 16:23:38 +0200

    init files

---
+++ CPP_07/ex01/iter.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/24 16:22:54 by thmeyer           #+#    #+#             */
+/*   Updated: 2023/10/24 16:23:24 by thmeyer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+# define BLACK "\033[0;30m"
+# define RED "\033[0;31m"
+# define GREEN "\033[0;32m"
+# define YELLOW "\033[0;33m"
+# define BLUE "\033[0;34m"
+# define PURPLE "\033[0;35m"
+# define CYAN "\033[0;36m"
+# define GRAY "\033[0;37m"
+# define RESET "\033[0m" 
+# define BOLD "\033[1m"
+
+#endif
