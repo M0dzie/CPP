bf11d45f4876de8b30d6c327ff09b560ed94efbd
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-11-07 12:49:37 +0100

    need to fix tests in main but iterator works

--- CPP_08/ex02/MutantStack.hpp
+++ CPP_08/ex02/MutantStack.hpp
@@ -6,17 +6,15 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 09:20:16 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/11/07 10:58:53 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/11/07 12:40:43 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 # include <algorithm>
-# include <deque>
 # include <stack>
 # include <list>
-# include <vector>
 # include <iostream>
 
 # define BLACK "\033[0;30m"
@@ -30,15 +28,30 @@
 # define RESET "\033[0m" 
 # define BOLD "\033[1m"
 
-template<typename T, typename Container = std::deque<T> >
-class MutantStack : public std::stack<T, Container>
+template<typename T>
+class MutantStack : public std::stack<T>
 {
 public:
-    MutantStack() : std::stack<T, Container>() {};
-    MutantStack(MutantStack const &rhs) : std::stack<T, Container>(rhs) {}
+    MutantStack() : std::stack<T>() {};
+    MutantStack(MutantStack const &rhs) : std::stack<T>(rhs) {}
     MutantStack &operator=(MutantStack const &rhs) {if (this != &rhs) this->c = rhs.c; return *this;}
     ~MutantStack() {}
 
+    typedef typename std::stack<T>::container_type::iterator iterator;
+    iterator begin() {return this->c.begin();}
+    iterator end() {return this->c.end();}
+
+    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+    const_iterator cbegin() {return this->c.cbegin();} 
+    const_iterator cend() {return this->c.cend();}
+
+    typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+    reverse_iterator rbegin() {return this->c.rbegin();}
+    reverse_iterator rend() {return this->c.rend();}
+
+    typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+    const_reverse_iterator crbegin() {return this->c.crbegin();}
+    const_reverse_iterator crend() {return this->c.crend();}
 };
 
 #endif
--- CPP_08/ex02/main.cpp
+++ CPP_08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 09:19:41 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/11/07 11:06:40 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/11/07 12:49:03 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,55 +17,68 @@ int main(void)
     std::cout << YELLOW << BOLD << "---- TESTING INHERITED MEMBER FUNCTION ----" << RESET << std::endl;
     {
         MutantStack<int> mstack;
+        int index = 0;
         std::cout << BLACK << "Is stack empty ? (1 = True, 0 = False): "  << RESET << mstack.empty() << std::endl;
         std::cout << BLACK << "Add 5 elements" << RESET << std::endl;
         for (int i = 0; i < 5; i++)
             mstack.push(i);
+        std::cout << BLACK << "Display stack : " << RESET << std::endl;
+        for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); it++)
+        {
+            std::cout << BLACK << index << "th element : " << RESET << *it << std::endl;
+            index++;
+        }
         std::cout << BLACK << "Size of the stack : " << RESET << mstack.size() << std::endl;
         std::cout << BLACK << "Access next element : " << RESET << mstack.top() << std::endl;
         std::cout << BLACK << "Remove top element" << RESET << std::endl;
         mstack.pop();
-        std::cout << BLACK << "Size of the stack : " << RESET << mstack.size() << std::endl;
+        index = 0;
+        std::cout << BLACK << "Display stack : " << RESET << std::endl;
+        for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); it++)
+        {
+            std::cout << BLACK << index << "th element : " << RESET << *it << std::endl;
+            index++;
+        }
     }
     std::cout << std::endl;
 
     std::cout << YELLOW << BOLD << "---- MANDATORY STACK TESTS ----" << RESET << std::endl;
     {
         MutantStack<int> mstack;
-        // std::cout << BLACK << "Add 2 elements" << RESET << std::endl;
-        // mstack.push(5);
-        // mstack.push(17);
-        // std::cout << BLACK << "Size of list : " << RESET << mstack.size() << std::endl;
-        // std::cout << BLACK << "First element : " << RESET << mstack.top() << std::endl;
-        // std::cout << BLACK << "Delete first element" << RESET << std::endl;
-        // mstack.pop();
-        // std::cout << BLACK << "Size of list : " << RESET << mstack.size() << std::endl;
-        // std::cout << BLACK << "Add 4 elements" << RESET << std::endl;
-        // mstack.push(3);
-        // mstack.push(5);
-        // mstack.push(737);
-        // //[...]
-        // mstack.push(0);
-        // MutantStack<int>::iterator it = mstack.begin();
-        // MutantStack<int>::iterator ite = mstack.end();
-        // ++it;
-        // --it;
-        // int index = 0;
-        // while (it != ite)
-        // {
-        //     std::cout << BLACK << index << "th element : " << RESET << *it << std::endl;
-        //     ++it;
-        //     index++;
-        // }
-        // std::cout << BLACK << "Copy of the list" << RESET << std::endl;
-        // std::stack<int> s(mstack);
-        // std::cout << BLACK << "Display of the new list" << RESET << std::endl;
-        // index = 0;
-        // for (std::list<int>::iterator it = s.begin(); it != s.end(); it++)
-        // {
-        //     std::cout << BLACK << index << "th element : " << RESET << *it << std::endl;
-        //     index++;
-        // }
+        std::cout << BLACK << "Add 2 elements" << RESET << std::endl;
+        mstack.push(5);
+        mstack.push(17);
+        std::cout << BLACK << "Size of list : " << RESET << mstack.size() << std::endl;
+        std::cout << BLACK << "First element : " << RESET << mstack.top() << std::endl;
+        std::cout << BLACK << "Delete first element" << RESET << std::endl;
+        mstack.pop();
+        std::cout << BLACK << "Size of list : " << RESET << mstack.size() << std::endl;
+        std::cout << BLACK << "Add 4 elements" << RESET << std::endl;
+        mstack.push(3);
+        mstack.push(5);
+        mstack.push(737);
+        mstack.push(0);
+        MutantStack<int>::iterator it = mstack.begin();
+        MutantStack<int>::iterator ite = mstack.end();
+        ++it;
+        --it;
+        int index = 0;
+        while (it != ite)
+        {
+            std::cout << BLACK << index << "th element : " << RESET << *it << std::endl;
+            ++it;
+            index++;
+        }
+        std::cout << BLACK << "Copy of the list" << RESET << std::endl;
+        std::stack<int> s(mstack);
+        std::cout << BLACK << "Display of the new list" << RESET << std::endl;
+        index = 0;
+        while (!s.empty())
+        {
+            std::cout << BLACK << index << "th element : " << RESET << s.top() << std::endl;
+            index++;
+            s.pop();
+        }
     }
     std::cout << std::endl;
 
@@ -73,19 +86,18 @@ int main(void)
     {
         std::list<int> mlist;
         std::cout << BLACK << "Add 2 elements" << RESET << std::endl;
-        mlist.push_back(5);
-        mlist.push_back(17);
+        mlist.push_front(5);
+        mlist.push_front(17);
         std::cout << BLACK << "Size of list : " << RESET << mlist.size() << std::endl;
         std::cout << BLACK << "First element : " << RESET << mlist.front() << std::endl;
         std::cout << BLACK << "Delete first element" << RESET << std::endl;
         mlist.pop_front();
         std::cout << BLACK << "Size of list : " << RESET << mlist.size() << std::endl;
         std::cout << BLACK << "Add 4 elements" << RESET << std::endl;
-        mlist.push_back(3);
-        mlist.push_back(5);
-        mlist.push_back(737);
-        //[...]
-        mlist.push_back(0);
+        mlist.push_front(3);
+        mlist.push_front(5);
+        mlist.push_front(737);
+        mlist.push_front(0);
         std::list<int>::iterator it = mlist.begin();
         std::list<int>::iterator ite = mlist.end();
         ++it;
