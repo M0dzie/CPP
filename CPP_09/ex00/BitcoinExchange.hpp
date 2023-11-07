248ce0c2e8b413f73dffbe86b7e948d7fca3de81
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-11-07 17:40:32 +0100

    first research

--- CPP_09/ex00/BitcoinExchange.hpp
+++ CPP_09/ex00/BitcoinExchange.hpp
@@ -6,12 +6,14 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 15:30:09 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/11/07 15:31:02 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/11/07 15:36:23 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BITCOINEXCHANGE_HPP
 # define BITCOINEXCHANGE_HPP
+# include <iostream>
+# include <string>
 
 # define BLACK "\033[0;30m"
 # define RED "\033[0;31m"
--- CPP_09/ex00/main.cpp
+++ CPP_09/ex00/main.cpp
@@ -6,13 +6,22 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 15:29:56 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/11/07 15:30:07 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/11/07 15:40:55 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "BitcoinExchange.hpp"
+#include "BitcoinExchange.hpp" 
 
-int main(void)
+static void displayErrorMessage(std::string msg)
 {
+    std::cerr << RED << msg << RESET << std::endl;
+}
+
+int main(int argc, char **argv)
+{
+    (void)argv;
+    if (argc != 2)
+        return (displayErrorMessage("Error: This program must take a single file as argument"), 1);
+
     return 0;
 }
