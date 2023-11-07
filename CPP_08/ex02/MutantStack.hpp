117db22c0cd5b1fca1364afaf28064bed7d574a3
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-11-07 09:49:07 +0100

    pimp tests in main

--- CPP_08/ex02/MutantStack.hpp
+++ CPP_08/ex02/MutantStack.hpp
@@ -6,12 +6,27 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 09:20:16 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/11/07 09:20:46 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/11/07 09:27:24 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
+# include <algorithm>
+# include <stack>
+# include <list>
+# include <iostream>
+
+# define BLACK "\033[0;30m"
+# define RED "\033[0;31m"
+# define GREEN "\033[0;32m"
+# define YELLOW "\033[0;33m"
+# define BLUE "\033[0;34m"
+# define PURPLE "\033[0;35m"
+# define CYAN "\033[0;36m"
+# define GRAY "\033[0;37m"
+# define RESET "\033[0m" 
+# define BOLD "\033[1m"
 
 class MutantStack
 {
--- CPP_08/ex02/main.cpp
+++ CPP_08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 09:19:41 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/11/07 09:19:53 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/11/07 09:48:45 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,82 @@
 
 int main(void)
 {
+    std::cout << YELLOW << BOLD << "---- MANDATORY STACK TESTS ----" << RESET << std::endl;
+    {
+        // MutantStack<int> mstack;
+        // std::cout << BLACK << "Add 2 elements" << RESET << std::endl;
+        // mstack.push(5);
+        // mstack.push(17);
+        // std::cout << BLACK << "Size of list : " << RESET << mstack.size() << std::endl;
+        // std::cout << BLACK << "First element : " << RESET << mstack.top() << std::endl;
+        // std::cout << BLACK << "Delete last element" << RESET << std::endl;
+        // mstack.pop();
+        // std::cout << BLACK << "Size of list : " << RESET << mstack.size() << std::endl;
+        // std::cout << BLACK << "Add 4 elements" << RESET << std::endl;
+        // mstack.push(3);
+        // mstack.push(5);
+        // mstack.push(737);
+        // //[...]
+        // mstack.push(0);
+        // MutantStack<int>::iterator it = mstack.begin();
+        // MutantStack<int>::iterator ite = mstack.end();
+        // ++it;
+        // --it;
+        // int index = 0;
+        // while (it != ite)
+        // {
+        //     std::cout << BLACK << index << "th element : " << RESET << *it << std::endl;
+        //     ++it;
+        //     index++;
+        // }
+        // std::cout << BLACK << "Copy of the list" << RESET << std::endl;
+        // std::stack<int> s(mstack);
+        // std::cout << BLACK << "Display of the new list" << RESET << std::endl;
+        // index = 0;
+        // for (std::list<int>::iterator it = s.begin(); it != s.end(); it++)
+        // {
+        //     std::cout << BLACK << index << "th element : " << RESET << *it << std::endl;
+        //     index++;
+        // }
+    }
+    std::cout << std::endl;
+    std::cout << YELLOW << BOLD << "---- MANDATORY LIST TESTS ----" << RESET << std::endl;
+    {
+        std::list<int> mlist;
+        std::cout << BLACK << "Add 2 elements" << RESET << std::endl;
+        mlist.push_back(5);
+        mlist.push_back(17);
+        std::cout << BLACK << "Size of list : " << RESET << mlist.size() << std::endl;
+        std::cout << BLACK << "First element : " << RESET << mlist.front() << std::endl;
+        std::cout << BLACK << "Delete last element" << RESET << std::endl;
+        mlist.pop_back();
+        std::cout << BLACK << "Size of list : " << RESET << mlist.size() << std::endl;
+        std::cout << BLACK << "Add 4 elements" << RESET << std::endl;
+        mlist.push_back(3);
+        mlist.push_back(5);
+        mlist.push_back(737);
+        //[...]
+        mlist.push_back(0);
+        std::list<int>::iterator it = mlist.begin();
+        std::list<int>::iterator ite = mlist.end();
+        ++it;
+        --it;
+        int index = 0;
+        while (it != ite)
+        {
+            std::cout << BLACK << index << "th element : " << RESET << *it << std::endl;
+            ++it;
+            index++;
+        }
+        std::cout << BLACK << "Copy of the list" << RESET << std::endl;
+        std::list<int> s(mlist);
+        std::cout << BLACK << "Display of the new list" << RESET << std::endl;
+        index = 0;
+        for (std::list<int>::iterator it = s.begin(); it != s.end(); it++)
+        {
+            std::cout << BLACK << index << "th element : " << RESET << *it << std::endl;
+            index++;
+        }
+    }
     return 0;
 }
