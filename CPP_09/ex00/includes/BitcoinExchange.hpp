98d6a122be2ef4031760f7a5f6cdf084680f8fa8
Thomas Meyer <thmeyer@student.42lyon.fr>
2023-11-09 10:55:22 +0100

    change error for wrong number of argument

--- CPP_09/ex00/includes/BitcoinExchange.hpp
+++ CPP_09/ex00/includes/BitcoinExchange.hpp
@@ -6,28 +6,13 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 15:30:09 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/11/09 10:39:47 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/11/09 10:50:04 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BITCOINEXCHANGE_HPP
 # define BITCOINEXCHANGE_HPP
-# include <iostream>
-# include <string>
-# include <fstream>
-# include <algorithm>
-# include <map>
-
-# define BLACK "\033[0;30m"
-# define RED "\033[0;31m"
-# define GREEN "\033[0;32m"
-# define YELLOW "\033[0;33m"
-# define BLUE "\033[0;34m"
-# define PURPLE "\033[0;35m"
-# define CYAN "\033[0;36m"
-# define GRAY "\033[0;37m"
-# define RESET "\033[0m" 
-# define BOLD "\033[1m"
+# include "data.hpp"
 
 class BitcoinExchange
 {
--- CPP_09/ex00/sources/main.cpp
+++ CPP_09/ex00/sources/main.cpp
@@ -6,12 +6,11 @@
 /*   By: thmeyer <thmeyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 15:29:56 by thmeyer           #+#    #+#             */
-/*   Updated: 2023/11/09 10:40:02 by thmeyer          ###   ########.fr       */
+/*   Updated: 2023/11/09 10:54:31 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/BitcoinExchange.hpp"
-#include "../includes/data.hpp"
 
 static void displayErrorMessage(std::string msg)
 {
@@ -30,9 +29,7 @@ static bool isValidFile(char const *argv)
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
-        return (displayErrorMessage("Error: This program must take a single file as argument."), 1);
-    if (!isValidFile(argv[1]) || !isValidFile("../data.csv"))
+    if (argc != 2 || !isValidFile(argv[1]) || !isValidFile("data.csv"))
         return (displayErrorMessage("Error: could not open file."), 1);
     if (!isDataBaseCorrect())
         return (displayErrorMessage("Error: Wrong format in database"), 1);
